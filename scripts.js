d6acf19f4fd0f12878d3729f10c7d00ce56307fc
angelo562 <username@example.com>
2022-06-26 22:35:21 -0700

    changed recipe.json simpler, working now with 3 recipes

--- scripts.js
+++ scripts.js
@@ -23,15 +23,42 @@ fetch("recipes.json")
         aTag.innerText = r.Name +" \n";
         myh3.appendChild(aTag);
 
+
+function stepTemplate(r2) {
+    return `
+    <h4 class=stepslist><h4>
+    <ul class=stepslist>
+        <li class=stepslist>${r2}</li>
+    </ul>
+    `}
+
+function ingredientsTemplate(i2) {
+    return `
     
+    <ul class=inglist>
+    <li>${i2}</li>
+    </ul>
+    
+    `
+}
+
+function recipeTemplate(r1) {
+    return `
+    <div class="ing_steps" id="${r1.Name}">
+        <h2>${r1.Name}</h2>
+        <p>Ingredients: ${r1.Ingredients.map(ingredientsTemplate).join("")}</p>
+        <p>Steps:${r1.Steps.map(stepTemplate).join("")}</p>
+
+    </div>
+    `}
 
 
-// document.getElementById("ingredientsList").innerHTML = `
 
-// ${data[IP Kimchi Stew]}
+document.getElementById("ingredientsList").innerHTML = `
 
-// <p class="footer">Footer</p>
-// `
+${data.map(recipeTemplate).join("")}         
+
+`
     
     
     })
